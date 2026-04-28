f0fa184af539c9d02996b7245e74595cc6043588
fazila7002 <fazilatunnisa705@gmail.com>
2026-04-28 20:50:41 +0700

    Nambahin variabel (atribut) ke dalam class dan bikin dia private

--- tugaspraktikum.cpp
+++ tugaspraktikum.cpp
@@ -4,6 +4,13 @@ using namespace std;
 class LayangLayang;
 
 class BelahKetupat {
+private:
+    double sisi;
+};
+
+class LayangLayang {
+private:
+    double d1, d2, sisi1, sisi2;
 };
 
 class LayangLayang {
